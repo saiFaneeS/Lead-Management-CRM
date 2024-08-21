a6b5781a5392e192aa5b5df3e12ac67c345cda80
Husban Baig <saifanees11@gmail.com>
2024-08-21 22:00:56 +0500

    new cors origin url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
-  "https://ms-crm.vercel.app/",
+  "https://ms-crm.vercel.app",
+  
 ];
 
 app.use(
